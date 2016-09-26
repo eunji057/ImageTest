67f84d481e485c740f18508deb63f52a1d74ee6c
Eunji Na <neg219@naver.com>
2016-09-26 20:53:14 +0900

    #3 Change Layout and set Layout

--- app/src/main/java/kr/hs/emirim/eunji057/imagetest/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/eunji057/imagetest/MainActivity.java
@@ -13,28 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new MyImage(getApplicationContext()));
+        setContentView(R.layout.activity_main);
     }
 
-    class MyImage extends View {
-        MyImage(Context context){
-            super(context);  //view를 상속받을 때 디폴트 생성자가 없어서 객체생성을 할 수 없기 때문에 사용자 정의형을 만들려면
-        }
-
-        @Override
-        protected void onDraw(Canvas canvas) {
-            super.onDraw(canvas);
-
-            Bitmap picture= BitmapFactory.decodeResource(getResources(), R.drawable.love);
-            float x= ((getWidth()-picture.getWidth())*0.5f);
-            float y=((getHeight()-picture.getHeight())*0.5f);
-            float cx=getWidth()/2.0f;
-            float cy=getHeight()/2.0f;
-            //canvas.rotate(180, cx, cy);
-            //canvas.translate(-150, 200);
-            //canvas.scale(2, 2, cx, cy);
-            canvas.skew(0.4f, 0.4f);
-            canvas.drawBitmap(picture, x, y, null);
-        }
-    }
 }
