fec39caf9dace7ae66868512655ef457b965f612
Eunji Na <neg219@naver.com>
2016-09-27 20:23:12 +0900

    #4 complete MyImage

--- app/src/main/java/kr/hs/emirim/eunji057/imagetest/MyImage.java
+++ app/src/main/java/kr/hs/emirim/eunji057/imagetest/MyImage.java
@@ -7,26 +7,44 @@ import android.graphics.Canvas;
 import android.view.View;
 
 /**
- * Created by appcreate09 on 2016. 9. 26..
+ * Created by appcreator16 on 2016. 9. 26..
  */
 public class MyImage extends View{
-    MyImage(Context context){
-        super(context);  //view를 상속받을 때 디폴트 생성자가 없어서 객체생성을 할 수 없기 때문에 사용자 정의형을 만들려면
+    int choose=MainActivity.ORIGINAL;
+    Bitmap picture;
+
+    MyImage(Context context) {
+        super(context);
+        picture= BitmapFactory.decodeResource(getResources(), R.drawable.love);
+    }
+    public void setChoose(int choose)
+    {
+        this.choose=choose;
     }
 
-//    @Override
-//    protected void onDraw(Canvas canvas) {
-//        super.onDraw(canvas);
-//
-//        Bitmap picture= BitmapFactory.decodeResource(getResources(), R.drawable.love);
-//        float x= ((getWidth()-picture.getWidth())*0.5f);
-//        float y=((getHeight()-picture.getHeight())*0.5f);
-//        float cx=getWidth()/2.0f;
-//        float cy=getHeight()/2.0f;
-//        //canvas.rotate(180, cx, cy);
-//        //canvas.translate(-150, 200);
-//        //canvas.scale(2, 2, cx, cy);
-//        canvas.skew(0.4f, 0.4f);
-//        canvas.drawBitmap(picture, x, y, null);
-//    }
+    protected void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+
+        float cx=getWidth()/2.0f;
+        float cy=getHeight()/2.0f;
+        float x=(getWidth()-picture.getWidth())/2.0f;
+        float y=(getHeight()-picture.getHeight())/2.0f;
+        switch (choose)
+        {
+            case MainActivity.ROTATE:
+                canvas.rotate(45,cx,cy); //회전
+                break;
+            case MainActivity.TRANSLATE:
+                canvas.translate(-150,200); //이동
+                break;
+            case MainActivity.SCALE:
+                canvas.scale(1.5f,1.5f,cx,cy); //크기
+                break;
+            case MainActivity.SKEW:
+                canvas.skew(0.4f,0.4f); // 비틀기
+                break;
+        }
+        canvas.drawBitmap(picture,x,y,null);
+        //picture.recycle();
+    }
 }
